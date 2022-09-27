959af49d3d7bc28266b7163b06cd8eb97baf0ff6
Rosie Sapphire <jayjaygames40@gmail.com>
2022-09-27 18:58:05 -0500

    Added a passthrough shader for the geometry on the model

--- res/shaders/model_geom.glsl
+++ res/shaders/model_geom.glsl
@@ -1,28 +1,13 @@
 #version 330 core
 
-#define POINT_LIGHT_MAX 32
-
 layout(triangles) in;
 layout(triangle_strip, max_vertices = 3) out;
-
-struct light_point_t {
-	vec3 pos;
-	vec3 ambient_color;
-	vec3 diffuse_color;
-	vec3 specular_color;
-	float constant;
-	float linear;
-	float quadratic;
-};
 
 out vec3 normal;
 out vec2 uv;
 out vec3 frag_pos;
 out float time;
 out float trip_intensity;
-out vec3 view_pos;
-out light_point_t light_points[POINT_LIGHT_MAX];
-out int light_points_count_current;
 
 in DATA {
 	vec3 normal;
@@ -31,46 +16,16 @@ in DATA {
 	float time;
 	float trip_intensity;
 	mat4 projection;
-	mat4 model;
-	vec3 view_pos;
-	light_point_t light_points[POINT_LIGHT_MAX];
-	int light_points_count_current;
 } data_in[];
 
 void main() {
-	/* calculate TBN matrix for normal calculations */
-	vec3 edge0 = gl_in[1].gl_Position.xyz - gl_in[0].gl_Position.xyz;
-	vec3 edge1 = gl_in[2].gl_Position.xyz - gl_in[0].gl_Position.xyz;
-	vec2 delta_uv0 = data_in[1].uv - data_in[0].uv;
-	vec2 delta_uv1 = data_in[2].uv - data_in[0].uv;
-
-	float inv_det = 1.0 / (delta_uv0.x * delta_uv1.y - delta_uv1.x * delta_uv0.y);
-
-	vec3 tangent = vec3(inv_det * (delta_uv1.y * edge0 - delta_uv0.y * edge1));
-	vec3 bitangent = vec3(inv_det * (-delta_uv1.x * edge0 + delta_uv0.x * edge1));
-
-	vec3 t = normalize(vec3(data_in[0].model * vec4(tangent, 0.0)));
-	vec3 b = normalize(vec3(data_in[0].model * vec4(bitangent, 0.0)));
-	vec3 n = normalize(vec3(data_in[0].model * vec4(cross(edge1, edge0), 0.0)));
-	mat3 tbn = transpose(mat3(t, b, n));
-
-	/* add all the shit to all the vertices */
 	for(int i = 0; i < 3; i++) {
-		gl_Position =    data_in[i].projection * gl_in[i].gl_Position;
-		normal =         data_in[i].normal;
-		uv =             data_in[i].uv;
-		frag_pos =       tbn * data_in[i].frag_pos;
-		time =           data_in[i].time;
+		gl_Position = data_in[i].projection * gl_in[i].gl_Position;
+		normal = data_in[i].normal;
+		uv = data_in[i].uv;
+		frag_pos = data_in[i].frag_pos;
+		time = data_in[i].time;
 		trip_intensity = data_in[i].trip_intensity;
-		view_pos =       tbn * data_in[i].view_pos;
-
-		for(int j = 0; j < POINT_LIGHT_MAX; j++) {
-			light_points[j] = data_in[i].light_points[j];
-			light_points[j].pos = tbn * data_in[i].light_points[j].pos;
-		}
-
-		light_points_count_current = data_in[i].light_points_count_current;
-
 		EmitVertex();
 	}
 	EndPrimitive();
--- res/shaders/model_vert.glsl
+++ res/shaders/model_vert.glsl
@@ -6,13 +6,6 @@ layout(location = 2) in vec2 a_uv;
 layout(location = 3) in ivec3 bone_ids;
 layout(location = 4) in vec3 weights;
 
-out vec3 normal;
-out vec2 uv;
-out vec3 frag_pos;
-
-out float o_time;
-out float o_trip_intensity;
-
 const int MAX_BONES = 50;
 
 uniform mat4 model;
@@ -24,6 +17,15 @@ uniform mat4 bone_matrices[MAX_BONES];
 uniform float time;
 uniform float trip_intensity;
 uniform int render_layer;
+
+out DATA {
+	vec3 normal;
+	vec2 uv;
+	vec3 frag_pos;
+	float time;
+	float trip_intensity;
+	mat4 projection;
+} data_out;
 
 float rand(float n) {
 	return fract(sin(n) * 43758.5453123);
@@ -50,18 +52,17 @@ void main() {
 		total_normal = vec4(mat3(transpose(inverse(model))) * a_normal, 0.0);
 	}
 
-	gl_Position = projection * view * model * total_position;
+	gl_Position = view * model * total_position;
 
 	vec2 trip_offset;
 	trip_offset.x = perlin_noise((time * 1.57) + gl_Position.y) * (trip_intensity / 5);
 	trip_offset.y = cos(time * 0.785 + gl_Position.z) * (trip_intensity / 5);
 	gl_Position += vec4(trip_offset, 0.0, 0.0) * float(render_layer == 0);
 
-	normal = total_normal.xyz;
-	uv = a_uv;
-	frag_pos = vec3(model * vec4(a_pos, 1.0));
-
-	/* TODO: Maybe make these uniforms in both shaders idk */
-	o_time = time;
-	o_trip_intensity = trip_intensity;
+	data_out.normal = total_normal.xyz;
+	data_out.uv = a_uv;
+	data_out.frag_pos = vec3(model * vec4(a_pos, 1.0));
+	data_out.time = time;
+	data_out.trip_intensity = trip_intensity;
+	data_out.projection = projection;
 }
