a76e39d9338e8bafd2c243f6912637743b935287
Rosie Sapphire <jayjaygames40@gmail.com>
2022-09-27 22:27:33 -0500

    Cleaned up naming for shaders a bit

--- res/shaders/model_geom.glsl
+++ res/shaders/model_geom.glsl
@@ -1,52 +1,68 @@
 #version 330 core
 
+#define POINT_LIGHT_MAX 32
+
 layout(triangles) in;
 layout(triangle_strip, max_vertices = 3) out;
 
-out vec3 normal;
-out vec2 uv;
-out vec3 frag_pos;
-out float time;
-out float trip_intensity;
+struct light_point_t {
+	vec3 pos;
+	vec3 ambient_color;
+	vec3 diffuse_color;
+	vec3 specular_color;
+	float constant;
+	float linear;
+	float quadratic;
+};
 
-in DATA {
-	vec3 normal;
-	vec2 uv;
-	vec3 frag_pos;
-	float time;
-	float trip_intensity;
-	mat4 projection;
-	mat4 model;
-} data_in[];
+in vec3 o_normal[];
+in vec2 uv[];
+in vec3 frag_pos[];
+in float o_time[];
+in float o_trip_intensity[];
+in mat4 o_projection[];
+in mat4 o_model[];
+// in int o_light_points_count_current[];
+/*
+in light_point_t o_light_points[POINT_LIGHT_MAX];
+*/
+
+out vec3 f_normal;
+out vec2 f_uv;
+out vec3 f_frag_pos;
+out float f_time;
+out float f_trip_intensity;
+// out int f_light_points_count_current;
+/*
+out light_point_t light_points[POINT_LIGHT_MAX];
+*/
 
 void main() {
-	/* calculate the TBN matrix for normal mapping */
+	/* calculate the TBN matrix for o_normal mapping */
 	vec3 edge0 = gl_in[1].gl_Position.xyz - gl_in[0].gl_Position.xyz;
 	vec3 edge1 = gl_in[2].gl_Position.xyz - gl_in[0].gl_Position.xyz;
-	vec2 delta_uv0 = data_in[1].uv - data_in[0].uv;
-	vec2 delta_uv1 = data_in[2].uv - data_in[0].uv;
+	vec2 delta_uv0 = uv[1] - uv[0];
+	vec2 delta_uv1 = uv[2] - uv[0];
 	float inverse_delta = 1.0 / (delta_uv0.x * delta_uv1.y - delta_uv1.x * delta_uv0.y);
 
 	vec3 tangent = vec3(inverse_delta * (delta_uv1.y * edge0 - delta_uv0.y * edge1));
 	vec3 bitangent = vec3(inverse_delta * (-delta_uv1.x * edge0 - delta_uv0.x * edge1));
 
-	vec3 t = normalize(vec3(data_in[0].model * vec4(tangent, 0.0)));
-	vec3 b = normalize(vec3(data_in[0].model * vec4(bitangent, 0.0)));
-	vec3 n = normalize(vec3(data_in[0].model * vec4(cross(edge1, edge0), 0.0)));
+	vec3 t = normalize(vec3(o_model[0] * vec4(tangent, 0.0)));
+	vec3 b = normalize(vec3(o_model[0] * vec4(bitangent, 0.0)));
+	vec3 n = normalize(vec3(o_model[0] * vec4(cross(edge1, edge0), 0.0)));
 	mat3 tbn = transpose(mat3(t, b, n));
 
 	for(int i = 0; i < 3; i++) {
-		gl_Position = data_in[i].projection * gl_in[i].gl_Position;
-		normal = data_in[i].normal;
-		uv = data_in[i].uv;
-		frag_pos = data_in[i].frag_pos;
-		time = data_in[i].time;
-		trip_intensity = data_in[i].trip_intensity;
+		gl_Position = o_projection[i] * gl_in[i].gl_Position;
+		f_normal = o_normal[i];
+		f_uv = uv[i];
+		f_frag_pos = frag_pos[i];
+		f_time = o_time[i];
+		f_trip_intensity = o_trip_intensity[i];
+		// f_light_points_count_current = o_light_points_count_current[i];
 		/*
-		for(int j = 0; j < POINT_LIGHT_MAX; j++) {
-			light_points[j] = data_in[i].light_points[j];
-		}
-		light_points_count_current = data_in[i].light_points_count_current;
+		light_points = data_in[i].light_points;
 		*/
 		EmitVertex();
 	}
--- res/shaders/model_vert.glsl
+++ res/shaders/model_vert.glsl
@@ -1,12 +1,13 @@
 #version 330 core
 
+#define POINT_LIGHT_MAX 32
 #define MAX_BONES 50
 
 layout(location = 0) in vec3 a_pos;
 layout(location = 1) in vec3 a_normal;
 layout(location = 2) in vec2 a_uv;
-layout(location = 3) in ivec3 bone_ids;
-layout(location = 4) in vec3 weights;
+layout(location = 3) in ivec3 a_bone_ids;
+layout(location = 4) in vec3 a_weights;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -18,23 +19,29 @@ uniform float time;
 uniform float trip_intensity;
 uniform int render_layer;
 
-/*
-out DATA {
-	vec3 normal;
-	vec2 uv;
-	vec3 frag_pos;
-	float o_time;
-	float o_trip_intensity;
-	mat4 projection;
-	mat4 model;
-} data_out;
-*/
+struct light_point_t {
+	vec3 pos;
+	vec3 ambient_color;
+	vec3 diffuse_color;
+	vec3 specular_color;
+	float constant;
+	float linear;
+	float quadratic;
+};
+uniform light_point_t light_points[POINT_LIGHT_MAX];
+// uniform int light_points_count_current;
 
-out vec3 normal;
-out vec2 uv;
-out vec3 frag_pos;
+out vec3 o_normal;
+out vec2 o_uv;
+out vec3 o_frag_pos;
 out float o_time;
 out float o_trip_intensity;
+out mat4 o_projection;
+out mat4 o_model;
+// out int o_light_points_count_current;
+/*
+out light_point_t o_light_points[POINT_LIGHT_MAX];
+*/
 
 float rand(float n) {
 	return fract(sin(n) * 43758.5453123);
@@ -51,39 +58,32 @@ void main() {
 	vec4 total_normal = vec4(0.0);
 	if(is_animated) {
 		for(int i = 0; i < 3; i++) {
-			vec4 local_position = bone_matrices[bone_ids[i]] * vec4(a_pos, 1.0);
-			vec4 world_normal = bone_matrices[bone_ids[i]] * vec4(a_normal, 0.0);
-			total_position += local_position * weights[i];
-			total_normal += world_normal * weights[i];
+			vec4 local_position = bone_matrices[a_bone_ids[i]] * vec4(a_pos, 1.0);
+			vec4 world_normal = bone_matrices[a_bone_ids[i]] * vec4(a_normal, 0.0);
+			total_position += local_position * a_weights[i];
+			total_normal += world_normal * a_weights[i];
 		}
 	} else {
 		total_position = vec4(a_pos, 1.0);
 		total_normal = vec4(mat3(transpose(inverse(model))) * a_normal, 0.0);
 	}
 
-	/*
 	gl_Position = view * model * total_position;
-	*/
-	gl_Position = projection * view * model * total_position;
 
 	vec2 trip_offset;
-	trip_offset.x = perlin_noise((o_time * 1.57) + gl_Position.y) * (o_trip_intensity / 5);
-	trip_offset.y = cos(o_time * 0.785 + gl_Position.z) * (o_trip_intensity / 5);
+	trip_offset.x = perlin_noise((time * 1.57) + gl_Position.y) * (trip_intensity / 5);
+	trip_offset.y = cos(time * 0.785 + gl_Position.z) * (trip_intensity / 5);
 	gl_Position += vec4(trip_offset, 0.0, 0.0) * float(render_layer == 0);
 
-	/*
-	data_out.normal = total_normal.xyz;
-	data_out.uv = a_uv;
-	data_out.frag_pos = vec3(model * vec4(a_pos, 1.0));
-	data_out.time = time;
-	data_out.trip_intensity = trip_intensity;
-	data_out.projection = projection;
-	data_out.model = model;
-	*/
-
-	normal = total_normal.xyz;
-	uv = a_uv;
-	frag_pos = vec3(model * vec4(a_pos, 1.0));
+	o_normal = total_normal.xyz;
+	o_uv = a_uv;
+	o_frag_pos = vec3(model * vec4(a_pos, 1.0));
 	o_time = time;
 	o_trip_intensity = trip_intensity;
+	/*
+	o_light_points = light_points;
+	*/
+	// o_light_points_count_current = light_points_count_current;
+	o_projection = projection;
+	o_model = model;
 }
